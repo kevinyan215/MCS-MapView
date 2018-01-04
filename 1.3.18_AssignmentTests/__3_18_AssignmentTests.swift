8c8f6d947d0205101cbec69f3b7ea65c944f7976
Kevin Yan <kevinyan215@gmail.com>
2018-01-03 20:06:11 -0500

    Initial Commit

---
+++ 1.3.18_AssignmentTests/__3_18_AssignmentTests.swift
@@ -0,0 +1,36 @@
+//
+//  __3_18_AssignmentTests.swift
+//  1.3.18_AssignmentTests
+//
+//  Created by Kevin Yan on 1/3/18.
+//  Copyright © 2018 Kevin Yan. All rights reserved.
+//
+
+import XCTest
+@testable import __3_18_Assignment
+
+class __3_18_AssignmentTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
